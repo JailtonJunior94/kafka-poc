3429be815db72b9dc947333fa07ed28bc4eafc93
Allan David <allanbrados@gmail.com>
2023-12-11 14:14:31 -0300

    add dependencies

--- samples/elixir/mix.exs
+++ samples/elixir/mix.exs
@@ -25,6 +25,9 @@ defmodule MyProducer.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:kaffe, "~> 1.24"},
+      {:protox, "~> 1.6"},
+      {:jason, "~> 1.2"},
+      {:confluent_schema_registry, "~> 0.1.1"}
     ]
   end
 end
