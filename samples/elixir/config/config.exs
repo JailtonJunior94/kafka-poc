ef6ab126431ef85ce4cd49f936e2636c66a47337
Allan David <allanbrados@gmail.com>
2023-12-11 15:00:43 -0300

    get the schema from schema registry

--- samples/elixir/config/config.exs
+++ samples/elixir/config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :kaffe,
   producer: [
     heroku_kafka_env: false,
-    endpoints: [localhost: 9094],
+    endpoints: [localhost: 9092],
     # endpoints references [hostname: port]. Kafka is configured to run on port 9092.
     # In this example, the hostname is localhost because we've started the Kafka server
     # straight from our machine. However, if the server is dockerized, the hostname will
--- samples/elixir/lib/my_producer/main.ex
+++ samples/elixir/lib/my_producer/main.ex
@@ -10,4 +10,19 @@ defmodule ExampleProducer do
   def send_my_message(value) do
     Kaffe.Producer.produce_sync("sample_key", value)
   end
+
+  def test() do
+    client = ConfluentSchemaRegistry.client(base_url: "http://localhost:8081/")
+    case ConfluentSchemaRegistry.get_schema(client, "poc-schemaregistry-value", "1") do
+      {:ok, reg} ->
+        IO.puts("Registro:")
+        IO.inspect(reg)
+      {:error, 404, %{"error_code" => 40401}} ->
+        IO.puts("Not Found")
+      {:error, 404, %{"error_code" => 40403}} ->
+        IO.puts("Not Found")
+      {:error, code, reason} ->
+        IO.puts("Other error: #{code} #{reason}")
+    end
+  end
 end
