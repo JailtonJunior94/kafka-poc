096a63211695f9f5afb7e2d3c9675e20226770c3
Allan David <allanbrados@gmail.com>
2023-12-11 20:08:41 -0300

    produce to topic

--- samples/elixir/config/config.exs
+++ samples/elixir/config/config.exs
@@ -8,5 +8,5 @@ config :kaffe,
     # In this example, the hostname is localhost because we've started the Kafka server
     # straight from our machine. However, if the server is dockerized, the hostname will
     # be called whatever is specified by that container (usually "kafka")
-    topics: ["our_topic", "another_topic"], # add a list of topics you plan to produce messages to
+    topics: ["poc-schemaregistry", "poc-schemaregistry-value"], # add a list of topics you plan to produce messages to
   ]
--- samples/elixir/lib/my_producer/main.ex
+++ samples/elixir/lib/my_producer/main.ex
@@ -11,18 +11,38 @@ defmodule ExampleProducer do
     Kaffe.Producer.produce_sync("sample_key", value)
   end
 
-  def test() do
-    client = ConfluentSchemaRegistry.client(base_url: "http://localhost:8081/")
-    case ConfluentSchemaRegistry.get_schema(client, "poc-schemaregistry-value", "1") do
-      {:ok, reg} ->
-        IO.puts("Registro:")
-        IO.inspect(reg)
-      {:error, 404, %{"error_code" => 40401}} ->
-        IO.puts("Not Found")
-      {:error, 404, %{"error_code" => 40403}} ->
-        IO.puts("Not Found")
-      {:error, code, reason} ->
-        IO.puts("Other error: #{code} #{reason}")
-    end
+  def produce(topic \\ "poc-schemaregistry") do
+    # Create a client connected to the schema registry
+    # client = ConfluentSchemaRegistry.client(base_url: "http://localhost:8081/")
+
+    msg = %Course.CourseMessage{id: "123", description: "teste"}
+          |> Course.CourseMessage.json_encode!()
+
+    message_object = %{
+      key: "1",
+      value: msg,
+      headers: [
+        {"Content-Type", "application/vnd.kafka.protobuf.v2+json"},
+        {"Accept", "application/vnd.kafka.v2+json"}
+      ]
+    }
+
+    Kaffe.Producer.produce(topic, [message_object])
+
+    # case ConfluentSchemaRegistry.get_schema(client, "poc-schemaregistry-value", "latest") do
+    #   {:ok, reg} ->
+    #     IO.puts("Registro:")
+    #     IO.inspect(reg)
+    #     IO.inspect(reg["schema"])
+
+    #   {:error, 404, %{"error_code" => 40401}} ->
+    #     IO.puts("Not Found")
+    #   {:error, 404, %{"error_code" => 40403}} ->
+    #     IO.puts("Not Found")
+    #   {:error, code, reason} ->
+    #     IO.inspect("Other error:")
+    #     IO.inspect(code)
+    #     IO.inspect(reason)
+    # end
   end
 end
